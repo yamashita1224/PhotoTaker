c105d20eb72dbb4368fd323c7dc3173ea4615d7a
Yamashita Satoshi <satoshi.yamashita1224@gmail.com>
2018-04-27 15:38:04 +0900

    写真の撮影とMainActivityでの表示を実装

--- app/src/main/java/jp/ac/titech/itpro/sdl/phototaker/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/phototaker/MainActivity.java
@@ -3,8 +3,9 @@ package jp.ac.titech.itpro.sdl.phototaker;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -26,8 +27,10 @@ public class MainActivity extends AppCompatActivity {
         photoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                // TODO: You should setup appropriate parameters for the intent
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                // intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.withAppendedPath(mLocationForPhotos, targetFilename));
+                startActivityForResult(intent, REQ_PHOTO);
                 PackageManager packageManager = getPackageManager();
                 List activities = packageManager
                         .queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
@@ -54,7 +57,8 @@ public class MainActivity extends AppCompatActivity {
         switch (reqCode) {
         case REQ_PHOTO:
             if (resCode == RESULT_OK) {
-                // TODO: You should implement the code that retrieve a bitmap image
+                photoImg = data.getParcelableExtra("data");
+                showPhoto();
             }
             break;
         }
